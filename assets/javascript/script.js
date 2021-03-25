711573823dfb08b9630eb51eaa5d3660d3334c6a
Bridget McCavanagh <blmccavanagh@gmail.com>
2021-03-25 23:59:48 +0800

    Alert windows added.
    To ensure password length is within the allowed parameters and that a minimum of one of the character types for the password has been selected.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -25,11 +25,21 @@ var charCodes = [];
 function generatePassword() {
 
     // Get the criteria from the user.
-    var passwordLength = prompt('How many characters would you like your password to contain?')
+    var passwordLength = getPasswordLength();
     var useLowercase = confirm("Include Lowercase: ok=yes cancel=no");
     var useUppercase = confirm("Include Uppercase: ok=yes cancel=no");
     var useNumbers = confirm("Include Numbers: ok=yes cancel=no");
     var useSymbols = confirm("Include Symbols: ok=yes cancel=no");
+
+    // Prompt the user to input the length of password they are trying to generate, if they provide a value outside the given parameters they will be redirected to begin again and enter a valid value.
+    function getPasswordLength() {
+      var passwordLength = prompt('How many characters would you like your password to contain? min: 8 max: 128');
+      while (passwordLength < 8 || passwordLength > 128 || isFinite(passwordLength) !== true) {
+        window.alert("You must enter a number between 8 and 128. Please try again!");
+        passwordLength = askForPasswordLength();
+      }
+      return passwordLength;
+    }
 
     // Concatenate the arrays from which password will be generated.
     if (useLowercase) {
@@ -46,6 +56,12 @@ function generatePassword() {
 
     if (useSymbols) {
         charCodes = charCodes.concat(SYMBOL_CHAR_CODES);  
+    }
+
+    // If the user selects none of the selection criteria they will receive an alert message to tell them that at least one of the options given must be chosen.
+    if (!useLowercase, !useUppercase, !useNumbers, !useSymbols) {
+      window.alert('At least one character type must be selected!');
+      return passwordCharacters = '';
     }
 
     var passwordCharacters = []
